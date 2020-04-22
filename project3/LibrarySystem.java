1ea28b5be548396fd31b7f6ea6e81130477a2f5a
Eric Lee <szl0094@auburn.edu>
2020-04-21 23:42:50 -0500

    fixes

--- project3/LibrarySystem.java
+++ project3/LibrarySystem.java
@@ -8,20 +8,10 @@ public class LibrarySystem {
         return new Menu();
     }
 
-    public void checkOut(Item item) {
-        if (item.getItemStatus() == 0) {
-           item.setItemStatus(1);
-           System.out.println("Item checked out. Returning to menu");
-        }
-        
-        else System.out.println("Item not available for checkout. Returning to menu");
-    }
-
     public void reserve(Item item) {
         if (item.getItemStatus() == 0) {
-            item.setItemStatus(2);
+            item.setItemStatus(1);
+            System.out.println("Item reserved. Returning to menu");
         }
-        else System.out.println("Item reserved. Returning to menu");
-
     }
 }
